34c522aa5197cd525a54a431807e78b764545344
gavrileyes <gavriley-es@mail.ru>
2021-10-06 07:45:30 +0300

    task 2...50%

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 // lesson 2( task 1)
-console.log("Record 1"); // синхронное исполнение №1
+// console.log("Record 1"); // синхронное исполнение №1
 
-setTimeout(() => {
-  // макротаска, исполнение №5
-  console.log("Record 2"); // синхронное исполнение в макротаске, №6
-  Promise.resolve().then(() => {
-    // микротаска в макротаске, исполнение №7
-    setTimeout(() => {
-      // макротаска в микротаске, исполнение №8
-      console.log("Record 3"); // синхронное исполнение в макротаске №9
-      Promise.resolve().then(() => {
-        console.log("Record 4"); // микротаска в макротаске, исполнение №10
-      });
-    });
-  });
-});
+// setTimeout(() => {
+//   // макротаска, исполнение №5
+//   console.log("Record 2"); // синхронное исполнение в макротаске, №6
+//   Promise.resolve().then(() => {
+//     // микротаска в макротаске, исполнение №7
+//     setTimeout(() => {
+//       // макротаска в микротаске, исполнение №8
+//       console.log("Record 3"); // синхронное исполнение в макротаске №9
+//       Promise.resolve().then(() => {
+//         console.log("Record 4"); // микротаска в макротаске, исполнение №10
+//       });
+//     });
+//   });
+// });
 
-console.log("Record 5"); // синхронное исполнение №2
+// console.log("Record 5"); // синхронное исполнение №2
 
-Promise.resolve().then(
-  () =>
-    // микротаска, исполнение №3
-    Promise.resolve().then(() => console.log("Record 6")) // микротаска в микротаске, исполнение №4
-);
+// Promise.resolve().then(
+//   () =>
+//     // микротаска, исполнение №3
+//     Promise.resolve().then(() => console.log("Record 6")) // микротаска в микротаске, исполнение №4
+// );
 
 // вывод в консоль:
 // Record 1
@@ -31,6 +31,124 @@ Promise.resolve().then(
 // Record 2
 // Record 3
 // Record 4
+
+// lesson 2( task 2)
+
+// Напишите программу, которая будет принимать на вход несколько аргументов: дату и время в формате
+// «час-день-месяц-год». Задача программы — создавать для каждого аргумента таймер с обратным отсчётом:
+// посекундный вывод в терминал состояния таймеров (сколько осталось). По истечении какого-либо таймера,
+// вместо сообщения о том, сколько осталось, требуется показать сообщение о завершении его работы.
+// Важно, чтобы работа программы основывалась на событиях.
+
+const readline = require("readline-sync");
+const EventEmitter = require("events");
+const emitter = new EventEmitter();
+
+
+function main() {
+  const timer = readline.question(
+    'Enter timer value in the format "hour-day-month-year"(13-05-10-2021):'
+  );
+  const [hour, day, month, year] = timer.split("-");
+  const timerStructure = {
+    hour,
+    day,
+    month,
+    year,
+    minutes: 0,
+    seconds: 0,
+  };
+
+  if (validationTimer(timerStructure)) {
+    console.log("The timer is entered incorrectly! Try again.");
+    return;
+  }
+
+  
+
+  
+  run(timerStructure)
+  
+
+  // showTimer(timerStructure);
+}
+
+function validationTimer({hour, day, month, year}) {
+  console.clear();
+
+  if (!hour || isNaN(+hour) || +hour > 23 || +hour < 0) {
+    console.log(`The hour ('${hour}') is specified incorrectly! (PM: 0..23)`);
+    return true;
+  }
+  if (!day || isNaN(+day) || +day > 31 || +day < 0) {
+    console.log(`The day ('${day}') is specified incorrectly! (0..31)`);
+    return true;
+  }
+  if (!month || isNaN(+month) || +month > 12 || +month < 0) {
+    console.log(`The month ('${month}') is specified incorrectly! (0..12)`);
+    return true;
+  }
+  if (!year || isNaN(+year) || +year > 9999 || +year < 0) {
+    console.log(`The year ('${year}') is specified incorrectly! (0..9999)`);
+    return true;
+  }
+}
+function showTimer({hour, day, month, year, minutes, seconds}) {
+  const timer = `year: ${year} month: ${month} day: ${day} hour: ${hour} minutes: ${minutes} seconds: ${seconds}`;
+  console.clear();
+
+  if (hour + day + month + year + minutes + seconds > 0) {
+    console.log(timer);
+  } else {
+    console.log("Time is over!");
+    return;
+  }
+}
+function calcSeconds(timerStructure) {
+
+  if( timerStructure.seconds === 0 ) {
+    timerStructure.seconds = 59;
+    emitter.emit('minutes',  calcMinutes(timerStructure));
+    return timerStructure;
+  } 
+  timerStructure.seconds--;
+
+  return timerStructure;
+};
+function calcMinutes(timerStructure) {
+
+  if( timerStructure.minutes === 0 ) {
+    timerStructure.minutes = 59;
+    return timerStructure;
+  }
+  timerStructure.minutes--;
+
+  return timerStructure;
+};
+function run(timerStructure) {
+
+  emitter.on('minutes', (timerStructure)=> {
+  });
+  emitter.on('second', (timerStructure)=> {
+    showTimer(timerStructure);
+  });
+  
+  emitter.on('hour', (timerStructure)=> {
+    showTimer(timerStructure);
+  });
+  emitter.on('day', (timerStructure)=> {
+    showTimer(timerStructure);
+  });
+  emitter.on('year', (timerStructure)=> {
+    showTimer(timerStructure);
+  });
+  const interval = setInterval(()=> emitter.emit('second',  calcSeconds(timerStructure)), 50);
+  // const interval = setInterval(()=> emitter.emit('second',  'text test'), 500);
+};
+
+main();
+
+
 
 // const colors = require("colors");
 // const readline = require("readline-sync");
