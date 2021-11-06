535e50326efb7e33c3d0e380f81390dc9f558941
gavrileyes <gavriley-es@mail.ru>
2021-11-06 13:26:01 +0300

    delete folder

--- multistreams/worker.js
+++
@@ -1,8 +0,0 @@
-const worker_threads = require('worker_threads');
-const crypto = require('crypto');
-
-const password = crypto.randomBytes(worker_threads.workerData).toString('hex');
-
-worker_threads.parentPort.postMessage({
-    password,
-});
