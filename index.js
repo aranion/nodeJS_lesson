6d4f58e1c731875b47e55efaa3631460027385a7
gavrileyes <gavriley-es@mail.ru>
2021-11-06 13:06:53 +0300

    finished leson 2(rewrote task 2)

--- index.js
+++ index.js
@@ -32,8 +32,6 @@
 // Record 3
 // Record 4
 
-
-
 // lesson 2( task 2)
 
 // Напишите программу, которая будет принимать на вход несколько аргументов: дату и время в формате
@@ -45,25 +43,38 @@
 const readline = require("readline-sync");
 const EventEmitter = require("events");
 const emitter = new EventEmitter();
+const moment = require("moment");
+require("moment-precise-range-plugin");
 
 // task 2
 function main() {
-  const timer = readline.question(
-    'Enter timer value in the format "hour-day-month-year"(13-05-10-2021):'
-  );
-  const [hour, day, month, year] = timer.split("-");
-  const timerStructure = {
-    hour,
-    day,
-    month,
-    year,
-    minutes: 0,
-    seconds: 0,
-  };
+  const dateNow = new Date();
+  const [hours, days, months, years] = readline
+    .question(
+      'Enter timer value in the format "hour-day-month-year"(13-05-10-2021):'
+    )
+    .split("-");
+  const timerStructure = {};
 
-  if (validationTimer(timerStructure)) {
-    console.log("The timer is entered incorrectly! Try again.");
+  if (new Date(years, months, days, hours, 0, 0) - dateNow <= 0) {
+    console.log("Please enter a date greater than this!");
     return;
+  } else {
+    if (validationTimer({ hours, days, months, years })) {
+      console.log("The timer is entered incorrectly! Try again.");
+      return;
+    }
+  }
+
+  const intervalDate = moment(dateNow).preciseDiff(
+    new Date(years, months, days, hours, 0, 0),
+    true
+  );
+
+  for (const key in intervalDate) {
+    if (Object.hasOwnProperty.call(intervalDate, key)) {
+      if (key !== "firstDateWasLater") timerStructure[key] = +intervalDate[key]
+    }
   }
 
   initialEmit();
@@ -73,29 +84,31 @@ function main() {
     showTimer(timerStructure);
   }, 1000);
 }
-function validationTimer({ hour, day, month, year }) {
+function validationTimer({ hours, days, months, years }) {
   console.clear();
 
-  if (!hour || isNaN(+hour) || +hour > 23 || +hour < 0) {
-    console.log(`The hour ('${hour}') is specified incorrectly! (PM: 0..23)`);
+  if (!hours || isNaN(+hours) || +hours > 23 || +hours < 0) {
+    console.log(`The hour ('${hours}') is specified incorrectly! (PM: 0..23)`);
     return true;
   }
-  if (!day || isNaN(+day) || +day > 31 || +day < 0) {
-    console.log(`The day ('${day}') is specified incorrectly! (0..31)`);
+  if (!days || isNaN(+days) || +days > 31 || +days < 0) {
+    console.log(`The day ('${days}') is specified incorrectly! (0..31)`);
     return true;
   }
-  if (!month || isNaN(+month) || +month > 12 || +month < 0) {
-    console.log(`The month ('${month}') is specified incorrectly! (0..12)`);
+  if (!months || isNaN(+months) || +months > 11 || +months < 0) {
+    console.log(`The month ('${months}') is specified incorrectly! (0..11)`);
     return true;
   }
-  if (!year || isNaN(+year) || +year > 9999 || +year < 0) {
-    console.log(`The year ('${year}') is specified incorrectly! (0..9999)`);
+  if (!years || isNaN(+years) || +years > 9999 || +years < 0) {
+    console.log(
+      `The year ('${years}') is specified incorrectly! (${new Date().getFullYear()}..9999)`
+    );
     return true;
   }
 }
 function showTimer(timerStructure) {
-  const { hour, day, month, year, minutes, seconds } = timerStructure;
-  const timer = `year: ${year} month: ${month} day: ${day} hour: ${hour} minutes: ${minutes} seconds: ${seconds}`;
+  const { hours, days, months, years, minutes, seconds } = timerStructure;
+  const timer = `year: ${years} month: ${months} day: ${days} hour: ${hours} minutes: ${minutes} seconds: ${seconds}`;
   console.clear();
 
   if (calcTimeIsOver(timerStructure) === 0) {
@@ -137,43 +150,43 @@ function calcMinutes(timer) {
   }
 }
 function calcHour(timer) {
-  if (timer.hour <= 0) {
-    timer.hour = 23;
+  if (timer.hours <= 0) {
+    timer.hours = 23;
     emitter.emit("day", timer);
   } else {
-    timer.hour--;
+    timer.hours--;
   }
 }
 function calcDay(timer) {
-  if (timer.day <= 0) {
-    timer.day = 30;
+  if (timer.days <= 0) {
+    timer.days = 30;
     emitter.emit("month", timer);
   } else {
-    timer.day--;
+    timer.days--;
   }
 }
 function calcMonth(timer) {
-  if (timer.month <= 0) {
-    timer.month = 11;
+  if (timer.months <= 0) {
+    timer.months = 11;
     emitter.emit("year", timer);
   } else {
-    timer.month--;
+    timer.months--;
   }
 }
 function calcYear(timer) {
-  if (timer.year <= 0) {
-    timer.year = 0;
+  if (timer.years <= 0) {
+    timer.years = 0;
   } else {
-    timer.year--;
+    timer.years--;
   }
 }
 function initialEmit() {
   emitter.on("seconds", (timerStructure) => calcSeconds(timerStructure));
   emitter.on("minutes", (timerStructure) => calcMinutes(timerStructure));
-  emitter.on("hour", (timerStructure) => calcHour(timerStructure));
-  emitter.on("day", (timerStructure) => calcDay(timerStructure));
-  emitter.on("month", (timerStructure) => calcMonth(timerStructure));
-  emitter.on("year", (timerStructure) => calcYear(timerStructure));
+  emitter.on("hours", (timerStructure) => calcHour(timerStructure));
+  emitter.on("days", (timerStructure) => calcDay(timerStructure));
+  emitter.on("months", (timerStructure) => calcMonth(timerStructure));
+  emitter.on("years", (timerStructure) => calcYear(timerStructure));
   emitter.on("endTimer", () => endTimer());
 }
 function endTimer() {
---
+++ multistreams/index.js
@@ -0,0 +1,20 @@
+const worker_threads = require('worker_threads');
+// SharedArrayBuffer
+
+const genPassword = (data) => {
+    return new Promise((resolve, reject) => {
+        const worker = new worker_threads.Worker('./worker.js', {
+            workerData: data,
+        });
+
+        worker.on('message', resolve);
+        worker.on('error', reject);
+    });
+};
+
+(async () => {
+    const passwordBytesSize = 4;
+    const password = await genPassword(passwordBytesSize);
+
+    console.log(password);
+})();
